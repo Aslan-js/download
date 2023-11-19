b2f86920ace926e9cfdde6d881bc74a08fe4cd7b
Aslan-js <140944897+Aslan-js@users.noreply.github.com>
2023-11-19 17:12:09 -0300

    Add files via upload

---
+++ download.js
@@ -0,0 +1,8 @@
+let desenvolvimentoCompleto = false;
+
+ while (!desenvolvimentoCompleto) {
+     alert('O jogo está em desenvolvimento. Aguarde a conclusão para mais informações.');
+     desenvolvimentoCompleto = confirm('Clique em OK para continuar a exibição da página.');
+ }
+
+ download.setAttribute('style', 'color: red; font-size: 25px;');
